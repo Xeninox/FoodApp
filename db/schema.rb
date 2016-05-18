bd0ada0bf7db5f55559ce1b98e000e9b56477d93
Likudie Constant <likudie@gmail.com>
2016-05-18 14:29:03 +0000

    Added contact to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160516210143) do
+ActiveRecord::Schema.define(version: 20160518141251) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20160516210143) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "contact"
   end
 
 end
